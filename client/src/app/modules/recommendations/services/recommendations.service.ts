02f0f17ea47952f1d5258342e6defda7f2326d08
Cody Peters <cjosephp15@gmail.com>
2022-12-11 23:03:50 +1000

    Rewrote filterGenres function

--- client/src/app/modules/recommendations/services/recommendations.service.ts
+++ client/src/app/modules/recommendations/services/recommendations.service.ts
@@ -4,12 +4,11 @@ import { QueryService } from 'src/app/shared/services/query.service';
 
 import {
   AverageSongFeatures,
-  Recommendation,
+  Recommendation
 } from 'src/app/shared/models/models';
 import {
-  FeaturesResponse,
-  RecommendationsResponse,
-  ArtistResponse,
+  ArtistResponse, FeaturesResponse,
+  RecommendationsResponse
 } from 'src/app/shared/models/spotify-models';
 
 @Injectable({
@@ -20,88 +19,80 @@ export class RecommendationsService {
 
   tracks: string[] = [];
   averageFeatures: AverageSongFeatures = new AverageSongFeatures();
-  filteredGenres: string[] = [];
+  genres: string[] = [];
+  genreSeeds: string[] = [];
   recommendations: Recommendation[] = [];
-  seed_method: string = 'genres';
 
   async filterGenres(genres: { genre: string; frequency: number }[]) {
-    // TODO: Refactor/rewrite this function
-    // Filter genres
+    // Sort genres by frequency
+    genres.sort((a, b) => b.frequency - a.frequency);
+
+    // Filter out genres that are less than iqr
+    var q1 = genres[Math.floor((genres.length * 3) / 4)].frequency;
+    var q3 = genres[Math.floor(genres.length / 4)].frequency;
+    var iqr = q3 - q1;
+    genres = genres.filter((genre) => genre.frequency > iqr);
+    this.genres = genres.map((genre) => genre.genre);
+    console.log('filter by iqr', iqr, genres);
+
+    // Get any valid genre seeds
     var url = `https://api.spotify.com/v1/recommendations/available-genre-seeds`;
-    var genre_seeds = <{ genres: string[] }>await this.query.get(url);
+    var availableGenreSeeds = <{ genres: string[] }>await this.query.get(url);
 
-    this.filteredGenres = [];
-    // Filter out genres that are not in the genre seeds or have a frequency of 1
     for (const genre of genres) {
-      if (genre.frequency > 1) {
-        if (genre_seeds.genres.includes(genre.genre)) {
-          this.filteredGenres.push(genre.genre);
-        }
+      if (availableGenreSeeds.genres.includes(genre.genre)) {
+        this.genreSeeds.push(genre.genre);
       }
     }
-    // If there are enough genres, continue to seed the recommendations with the top 5 genres
-    // Else, recommendations will be seeded with tracks and compared with genres
-    if (this.filteredGenres.length > 3) {
-      this.filteredGenres = this.filteredGenres.slice(0, 5);
-    } else {
-      this.seed_method = 'tracks';
-      this.filteredGenres = [];
-
-      for (const genre of genres) {
-        if (genre.frequency > 1) {
-          this.filteredGenres.push(genre.genre);
-        }
-      }
-
-      // If there are too few genres (due to a small playlist), add all genres including those with a frequency of 1
-      if (this.filteredGenres.length <= 3) {
-        this.filteredGenres = [];
-
-        for (const genre of genres) {
-          this.filteredGenres.push(genre.genre);
-        }
-      }
-    }
+    console.log('genre seeds', this.genreSeeds);
   }
 
   async fetchRecommendations() {
-    // Get recommendations
-    var seed = '';
+    // Get seed for recommendations
+    var seeds: { genre: string[]; track: string[] } = { genre: [], track: [] };
 
-    if (this.seed_method === 'genres') {
-      var genresTemp = this.filteredGenres.slice(0, 5);
-      seed = `seed_genres=${genresTemp.join(',')}`;
-    } else {
-      var tracksTemp = this.tracks.slice(0, 5);
-      seed = `seed_tracks=${tracksTemp.join(',')}`;
+    if (this.genreSeeds.length > 0) {
+      seeds.genre = this.genreSeeds.slice(0, 2);
+      this.genreSeeds = this.genreSeeds
+        .slice(seeds.genre.length)
+        .concat(this.genreSeeds.slice(0, seeds.genre.length));
     }
 
+    seeds.track = this.tracks.slice(0, 5 - seeds.genre.length);
+    this.tracks = this.tracks
+      .slice(seeds.track.length)
+      .concat(this.tracks.slice(0, seeds.track.length));
+
+    var seed = '';
+    if (seeds.genre.length > 0) {
+      seed = `seed_genres=${seeds.genre.join(',')}&`;
+    }
+    seed += `seed_tracks=${seeds.track.join(',')}`;
+
+    // Get recommendations
     var filters = `target_acousticness=${this.averageFeatures.acousticness}&target_danceability=${this.averageFeatures.danceability}&target_energy=${this.averageFeatures.energy}&target_instrumentalness=${this.averageFeatures.instrumentalness}&target_liveness=${this.averageFeatures.liveness}&target_loudness=${this.averageFeatures.loudness}&target_speechiness=${this.averageFeatures.speechiness}&target_tempo=${this.averageFeatures.tempo}&target_valence=${this.averageFeatures.valence}`;
     var url = `https://api.spotify.com/v1/recommendations?${seed}&limit=10&${filters}`;
+    console.log(url);
     var recommendations = <RecommendationsResponse>await this.query.get(url);
 
     for (const track of recommendations.tracks) {
       if (!this.tracks.includes(track.id)) {
         // Check if track has familiar genres
-        if (this.seed_method !== 'genres') {
-          var genreExists = false;
-          for (const artist of track.artists) {
-            var url = `https://api.spotify.com/v1/artists/${artist.id}`;
-            var artistData = <ArtistResponse>await this.query.get(url);
-            var genres = artistData.genres;
+        var genreExists = false;
+        for (const artist of track.artists) {
+          var url = `https://api.spotify.com/v1/artists/${artist.id}`;
+          var artistData = <ArtistResponse>await this.query.get(url);
+          var genres = artistData.genres;
 
-            const exists = genres.some(
-              (g) => this.filteredGenres.indexOf(g) >= 0
-            );
-            if (exists) {
-              genreExists = true;
-              break;
-            }
+          const exists = genres.some((g) => this.genres.indexOf(g) >= 0);
+          if (exists) {
+            genreExists = true;
+            break;
           }
-          if (!genreExists) {
-            console.log('No familiar genres');
-            continue;
-          }
+        }
+        if (!genreExists) {
+          console.log('No familiar genres');
+          continue;
         }
 
         // Compare song features with average song features
