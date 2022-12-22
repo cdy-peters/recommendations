c10bd3a4d8464532715f5aff94a27f7f461baf66
Cody Peters <cjosephp15@gmail.com>
2022-12-22 21:27:48 +1000

    Added error handing for invalid token

--- client/src/app/shared/services/query.service.ts
+++ client/src/app/shared/services/query.service.ts
@@ -1,28 +1,60 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 import { CookieService } from './cookie.service';
-import { lastValueFrom } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QueryService {
-  constructor(private http: HttpClient, private cookies: CookieService) {}
+  constructor(
+    private http: HttpClient,
+    private cookies: CookieService,
+    private auth: AuthService
+  ) {}
 
+  async errorHandler(status: number) {
+    switch (status) {
+      // Invalid access token
+      case 401:
+        var refresh_token = this.cookies.getCookie('refresh_token');
+        if (refresh_token) {
+          await this.auth.refreshToken(refresh_token);
+        } else {
+          this.auth.login();
+        }
+    }
+  }
+
+  // @ts-ignore
   async get(url: string) {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.cookies.getCookie('access_token')}`,
     });
 
-    return await lastValueFrom(this.http.get(url, { headers }));
+    try {
+      const res = await lastValueFrom(this.http.get(url, { headers }));
+      return res;
+    } catch (err: any) {
+      await this.errorHandler(err.status);
+      return await this.get(url);
+    }
   }
 
+  // @ts-ignore
   async post(url: string, body: any) {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.cookies.getCookie('access_token')}`,
     });
 
-    return await lastValueFrom(this.http.post(url, body, { headers }));
+    try {
+      const res = await lastValueFrom(this.http.post(url, body, { headers }));
+      return res;
+    } catch (err: any) {
+      await this.errorHandler(err.status);
+      return await this.post(url, body);
+    }
   }
 }
