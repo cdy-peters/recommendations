1bd1fa9cbadda23f029f50703da5b92393f5d812
Cody Peters <cjosephp15@gmail.com>
2022-12-26 14:01:29 +1000

    Added scroll to top button

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  windowScrolled: boolean = false;
   title = 'recommendations';
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset >= window.innerHeight / 2;
+    });
+  }
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 }
